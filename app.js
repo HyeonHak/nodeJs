edfda696920477cb1899371f41960cc2d90ce0d7
jhhak <jhhak@aprd.io>
2021-07-24 10:38:24 +0900

    Add xhr

--- app.js
+++ app.js
@@ -24,3 +24,8 @@ app.post('/email_post',function(req,res){
     //res.send("<h1>welcome!</h1>" + req.body.email);
     res.render('email.ejs', {'email': req.body.email});
 })
+
+app.post('/ajaxSendEmail',function(req,res){
+    console.log(req.body.email);
+    res.send({result: 'ok', email: req.body.email});
+})
